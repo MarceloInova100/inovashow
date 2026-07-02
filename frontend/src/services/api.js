77501600801adaeb7558f35602dbbb3035d91467
MarceloInova100 <marcelo.lima@inova100.com.br>
2026-07-02 11:56:13 -0300

    Configurando a API e o APP

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000'
+    baseURL: 'http://localhost:3000/'
 });
 
 export default api;
@@ -11,7 +11,7 @@ export default api;
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000'
+    baseURL: 'http://localhost:3000/'
 });
 
 export default api;
