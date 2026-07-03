4acbaa3a1b6333af9a8a5448d156e8ff1be77449
Marcelo Marques <marcelo.lima@inova100.com.br>
2026-07-03 15:23:51 -0300

    Telas de vendedor e pessoa

--- frontend/src/app/pessoa.tsx
+++ frontend/src/app/pessoa.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import PessoaForm from '../components/PassoaForm';
 
 export default function pessoa() {
   return (
-    <View>
-      <Text>Cadastro de Pessoas</Text>
-    </View>
+    <PessoaForm />
   );
 }
--- frontend/src/app/vendedor.tsx
+++ frontend/src/app/vendedor.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import VendedorForm from '../components/VendedorForm';
 
 export default function VendedorScreen() {
   return (
-    <View>
-      <Text>Cadastro de Vendedores</Text>
-    </View>
+    <VendedorForm />
   );
 }
